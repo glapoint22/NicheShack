a516e0f93329fc96d6879a08c8417cd60bc6834e
Gabe LaPoint <glapoint22@gmail.com>
2019-09-29 20:22:42 -0400

    Initial

---
+++ projects/website/src/app/shared-components/header-footer/navfooter/navfooter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navfooter',
+  templateUrl: './navfooter.component.html',
+  styleUrls: ['./navfooter.component.scss']
+})
+export class NavfooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
