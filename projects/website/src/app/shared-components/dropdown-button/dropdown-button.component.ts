ac71a1ccc3eec63b083855d204fd141f559e08f1
Gabe LaPoint <glapoint22@gmail.com>
2019-10-02 20:53:39 -0400

    Styling navbar

--- projects/website/src/app/shared-components/dropdown-button/dropdown-button.component.ts
+++ projects/website/src/app/shared-components/dropdown-button/dropdown-button.component.ts
@@ -1,38 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { KeyValue } from '@angular/common';
 
 @Component({
   selector: 'dropdown-button',
   templateUrl: './dropdown-button.component.html',
   styleUrls: ['./dropdown-button.component.scss']
 })
-export class DropdownButtonComponent {
-  private isMouseDown: boolean;
+export class DropdownButtonComponent implements OnChanges {
+  @Input() isRounded: boolean;
+  @Input() items: Array<KeyValue<string, string>>;
+  @Output() itemClick: EventEmitter<KeyValue<string, string>> = new EventEmitter();
+  public isMouseDown: boolean;
+  public showDropdown: boolean;
+  public caption: string;
+
+  ngOnChanges(): void {
+    if(this.items.length > 0) {
+      this.caption = this.items[0].value;
+    }
+  }
 
 
   onClick(dropdown: HTMLElement) {
-    if(this.isMouseDown) {
+    // Don't show the dropdown if there was a mousedown event
+    // This prevents the dropdown from showing when the button is clicked again
+    if (this.isMouseDown) {
       this.isMouseDown = false;
       return;
     }
 
+    // show the dropdown and set the focus
+    this.showDropdown = true;
     dropdown.focus();
-    dropdown.style.setProperty('max-height', '200px');
   }
 
   onKeydown(event: KeyboardEvent, dropdown: HTMLElement) {
+    // If escape is pressed, hide the dropdown
     if (event.code === 'Escape' || event.keyCode === 27) {
-      dropdown.style.setProperty('max-height', '0');
+      this.showDropdown = false;
       dropdown.blur();
     }
-
   }
 
-  onBlur(dropdown: HTMLElement) {
-    dropdown.style.setProperty('max-height', '0');
-  }
-
-  onMousedown(dropdown: HTMLElement) {
-    if(dropdown.style.getPropertyValue('max-height') != '0px' && dropdown.style.getPropertyValue('max-height') != "")
-    this.isMouseDown = true;
+  onItemClick(item: KeyValue<string, string>) {
+    this.itemClick.emit(item);
+    this.caption = item.value;
+    this.showDropdown = false;
   }
 }
--- projects/website/src/app/shared-components/header-footer/navbar/navbar.component.ts
+++ projects/website/src/app/shared-components/header-footer/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { KeyValue } from '@angular/common';
 
 @Component({
   selector: 'navbar',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public searchCategories: Array<KeyValue<string, string>> = []
 
   constructor() { }
 
   ngOnInit() {
+    this.searchCategories = [
+      {
+        key: '0',
+        value: 'Health & Fitness'
+      },
+      {
+        key: '1',
+        value: 'Self-Help'
+      },
+      {
+        key: '2',
+        value: 'E-business & E-marketing'
+      }
+    ]
   }
 
 }
