7dfad86038bcf4ad003856b9eb46d71073b58cb5
Gabe LaPoint <glapoint22@gmail.com>
2019-10-07 17:43:16 -0400

    Working on category menu

--- projects/website/src/app/shared-components/header-footer/navbar/category-menu/category-menu.component.ts
+++ projects/website/src/app/shared-components/header-footer/navbar/category-menu/category-menu.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { CategoriesService } from 'projects/website/src/app/services/categories.service';
+import { Category } from 'projects/website/src/app/interfaces/category';
+import { Niche } from 'projects/website/src/app/interfaces/niche';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'category-menu',
@@ -8,10 +12,29 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class CategoryMenuComponent implements OnInit {
   public show: boolean;
   private isMouseDown: boolean;
+  public categories: Array<Category>;
+  public niches: Array<Niche>;
+  public currentCategory: Category;
+  public nicheView: boolean;
 
   @ViewChild('menu', {static: false}) menu: ElementRef;
-
   
+
+  constructor(private categoriesService: CategoriesService, private router: Router) { }
+
+  ngOnInit() {
+    this.categoriesService.categories.subscribe((categories: Array<Category>) => {
+      this.categories = categories;
+    });
+  }
+
+  onKeydown(event: KeyboardEvent, dropdown: HTMLElement) {
+    // If escape is pressed, hide the menu
+    if (event.code === 'Escape' || event.keyCode === 27) {
+      this.show = false;
+      dropdown.blur();
+    }
+  }
 
   onClick() {
     if (this.isMouseDown) {
@@ -19,7 +42,8 @@ export class CategoryMenuComponent implements OnInit {
       return;
     }
 
-    // show the dropdown and set the focus
+    // show the menu and set the focus
+    this.nicheView = false;
     this.show = true;
     this.menu.nativeElement.focus();
   }
@@ -30,22 +54,19 @@ export class CategoryMenuComponent implements OnInit {
     } else {
       this.isMouseDown = false;
     }
-
-    
-  }
-  
-
-  constructor() { }
-
-  ngOnInit() {
   }
 
-  onKeydown(event: KeyboardEvent, dropdown: HTMLElement) {
-    // If escape is pressed, hide the dropdown
-    if (event.code === 'Escape' || event.keyCode === 27) {
-      this.show = false;
-      dropdown.blur();
-    }
+  onCategoryClick(categoryId: number) {
+    this.currentCategory = this.categories[categoryId];
+    this.niches = this.categories[categoryId].niches;
+    this.nicheView = true;
+  }
+
+  onNicheClick(nicheId: number) {
+    this.router.navigate(['/search'], {
+      queryParams: { 'categoryId': this.currentCategory.id, 'nicheId': nicheId }
+    });
+    this.show = false;
   }
 
 }
