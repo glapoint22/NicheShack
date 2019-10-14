4a4ce38e469c91de81624b0a160a06e7845dd171
Gabe LaPoint <glapoint22@gmail.com>
2019-10-14 14:14:32 -0400

    Added page component

--- projects/website/src/app/pages/home/home.component.ts
+++ projects/website/src/app/pages/home/home.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { DataService } from 'services/data.service';
 import { ProductGroup } from '../../interfaces/product-group';
+import { PageComponent } from '../page/page.component';
+import { Title, Meta } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent extends PageComponent implements OnInit {
   public productGroups: Array<ProductGroup>;
 
-  constructor(private dataService: DataService) { }
+  constructor(titleService: Title, metaService: Meta, @Inject(DOCUMENT) document, private dataService: DataService) {
+    super(titleService, metaService, document);
+  }
 
   ngOnInit() {
+    this.title = 'What\'s your niche?';
+    this.description = 'Online shopping from the largest affiliate marketing site on the planet, promoting items from thousands of companies and individuals.';
+    this.image = '/Images/tlou2.jpg';
+
+    super.ngOnInit();
     this.dataService.get('api/Home')
       .subscribe(response => {
         this.productGroups = response.productGroups;
       });
   }
-
-
 }
---
+++ projects/website/src/app/pages/page/page.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  template: ''
+})
+export class PageComponent implements OnInit {
+  // Properties for each page
+  public title: string;
+  public description: string;
+  public image: string;
+
+  // Other properties
+  public facebookAppId: string = '1399604666855152';
+  public domain: string;
+  public share: boolean = true;
+
+  constructor(private titleService: Title, private metaService: Meta, @Inject(DOCUMENT) private document) { }
+
+  ngOnInit() {
+    // Title
+    this.title = 'Niche Shack - ' + this.title;
+    this.domain = this.document.location.protocol + '//' + this.document.location.host;
+
+    // Title tag
+    this.titleService.setTitle(this.title);
+
+    // Description tag
+    if (this.description)
+        this.metaService.addTag({ name: 'description', content: this.description });
+
+    if (this.share) {
+        // Facebook
+        this.metaService.addTag({ property: 'og:title', content: this.title });
+        this.metaService.addTag({ property: 'og:site_name', content: 'Niche Shack' });
+        this.metaService.addTag({ property: 'og:type', content: 'website' });
+        this.metaService.addTag({ property: 'og:locale', content: 'en_US' });
+        this.metaService.addTag({ property: 'fb:app_id', content: this.facebookAppId });
+        this.metaService.addTag({ property: 'og:url', content: this.document.location.href });
+        this.metaService.addTag({ property: 'og:image', content: this.domain + this.image });
+        this.metaService.addTag({ property: 'og:image:width', content: '600' });
+        this.metaService.addTag({ property: 'og:image:height', content: '315' });
+        this.metaService.addTag({ property: 'og:description', content: this.description });
+
+        // Twitter
+        this.metaService.addTag({ name: 'twitter:card', content: 'summary' });
+        this.metaService.addTag({ name: 'twitter:site', content: '@Niche_Shack' });
+        this.metaService.addTag({ name: 'twitter:title', content: this.title });
+        this.metaService.addTag({ name: 'twitter:description', content: this.description });
+        this.metaService.addTag({ name: 'twitter:image', content: this.domain + this.image });
+    }
+  }
+
+}
