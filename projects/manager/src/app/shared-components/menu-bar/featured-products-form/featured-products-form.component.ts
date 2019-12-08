91c33ed220a2ceb1c3145cde3c8cf586c30dece2
Gabe LaPoint <glapoint22@gmail.com>
2019-12-08 18:56:06 -0500

    Completed featured products

---
+++ projects/manager/src/app/shared-components/menu-bar/featured-products-form/featured-products-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'featured-products-form',
+  templateUrl: './featured-products-form.component.html',
+  styleUrls: ['./featured-products-form.component.scss']
+})
+export class FeaturedProductsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/manager/src/app/shared-components/menu-bar/menu-bar.module.ts
+++ projects/manager/src/app/shared-components/menu-bar/menu-bar.module.ts
@@ -3,14 +3,25 @@ import { CommonModule } from '@angular/common';
 import { MenuBarComponent } from './menu-bar.component';
 import { MessageFormComponent } from './message-form/message-form.component';
 import { ProductReportFormComponent } from './product-report-form/product-report-form.component';
+import { FeaturedProductsFormComponent } from './featured-products-form/featured-products-form.component';
 
 
 
 @NgModule({
-  declarations: [MenuBarComponent, MessageFormComponent, ProductReportFormComponent],
+  declarations: [
+    MenuBarComponent, 
+    MessageFormComponent, 
+    ProductReportFormComponent,
+    FeaturedProductsFormComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: [MenuBarComponent, MessageFormComponent, ProductReportFormComponent]
+  exports: [
+    MenuBarComponent, 
+    MessageFormComponent, 
+    ProductReportFormComponent,
+    FeaturedProductsFormComponent
+  ]
 })
 export class MenuBarModule { }
