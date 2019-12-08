271425b963da44f9059ce3a559b70fb5bcecbfcf
Gabe LaPoint <glapoint22@gmail.com>
2019-12-08 12:35:20 -0500

    Completed menu bar

--- projects/manager/src/app/pages/niche-shack-editor/niche-shack-editor.module.ts
+++ projects/manager/src/app/pages/niche-shack-editor/niche-shack-editor.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NicheShackEditorRoutingModule } from './niche-shack-editor-routing.module';
 import { NicheShackEditorComponent } from './niche-shack-editor.component';
 import { DialogBoxModule } from '../../shared-components/dialog-box/dialog-box.module';
+import { MenuBarModule } from '../../shared-components/menu-bar/menu-bar.module';
 
 
 @NgModule({
   declarations: [NicheShackEditorComponent],
-  imports: [CommonModule, NicheShackEditorRoutingModule, DialogBoxModule]
+  imports: [CommonModule, NicheShackEditorRoutingModule, DialogBoxModule, MenuBarModule]
 })
 export class NicheShackEditorModule { }
