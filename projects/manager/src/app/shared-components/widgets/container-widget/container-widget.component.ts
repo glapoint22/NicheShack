ff3c24cb0805be6f95d71478ad4720210f3c4623
Gabe LaPoint <glapoint22@gmail.com>
2019-12-24 22:26:25 -0500

    Completed widgets

---
+++ projects/manager/src/app/shared-components/widgets/container-widget/container-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'container-widget',
+  templateUrl: './container-widget.component.html',
+  styleUrls: ['./container-widget.component.scss']
+})
+export class ContainerWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
