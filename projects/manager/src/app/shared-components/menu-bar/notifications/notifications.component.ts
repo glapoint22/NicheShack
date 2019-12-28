0ed4d7ad2666b24da260a0f05713d3df244e96ac
Gabe LaPoint <glapoint22@gmail.com>
2019-12-27 21:45:38 -0500

    Completed navigation & notifications menu

--- projects/manager/src/app/shared-components/menu-bar/notifications/notifications.component.ts
+++ projects/manager/src/app/shared-components/menu-bar/notifications/notifications.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent implements OnInit {
+  public show: boolean;
 
   constructor() { }
 
