ed156aced8a71bdeb6ad4c7b0d308428a25d50dd
Gabe LaPoint <glapoint22@gmail.com>
2020-05-14 19:16:47 -0400

    Completed columns property

--- projects/manager/src/app/shared-components/widget-properties/button-widget-properties/button-widget-properties.component.ts
+++ projects/manager/src/app/shared-components/widget-properties/button-widget-properties/button-widget-properties.component.ts
@@ -1,19 +1,13 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ButtonWidgetComponent } from '../../designer/widgets/button-widget/button-widget.component';
 import { ButtonState } from '../../../classes/button-state';
-import { WidgetComponent } from '../../designer/widgets/widget/widget.component';
 
 @Component({
   selector: 'button-widget-properties',
   templateUrl: './button-widget-properties.component.html',
   styleUrls: ['./button-widget-properties.component.scss']
 })
-export class ButtonWidgetPropertiesComponent implements OnChanges {
-  @Input() widget: WidgetComponent;
-  public button: ButtonWidgetComponent;
+export class ButtonWidgetPropertiesComponent {
+  @Input() buttonWidget: ButtonWidgetComponent;
   public buttonState = ButtonState;
-  
-  ngOnChanges() {
-    this.button = this.widget as ButtonWidgetComponent;
-  }
 }
